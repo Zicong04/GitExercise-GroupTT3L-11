8696d15083e1d90df53bdf65335946fd5f9c57a1
Haoowwww <jianhao030205@gmail.com>
2024-05-08 15:27:11 +0800

    hi

---
+++ membership/process_signup.php
@@ -0,0 +1,5 @@
+<?php
+
+header("Location: thank_you.php");
+exit();
+?>
---
+++ membership/thank_you.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Thank You</h1>
+    <p>Thank you for signing up!</p>
+</div>
+
+</body>
+</html>
