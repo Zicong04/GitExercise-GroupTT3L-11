a325ea3e765748153360d5a38b5a7146dc662dd8
Haoowwww <167295066+Haoowwww@users.noreply.github.com>
2024-06-11 21:27:13 +0800

    Add files via upload

---
+++ GitExercise-GroupTT3L-11-/users_db/code.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+require 'dbcon.php';
+
+if(isset($_POST['delete_student']))
+{
+    $student_id = mysqli_real_escape_string($con, $_POST['delete_student']);
+
+    $query = "DELETE FROM students WHERE id='$student_id' ";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run)
+    {
+        $_SESSION['message'] = "Student Deleted Successfully";
+        header("Location: index.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "Student Not Deleted";
+        header("Location: index.php");
+        exit(0);
+    }
+}
+
+if(isset($_POST['update_student']))
+{
+    $student_id = mysqli_real_escape_string($con, $_POST['student_id']);
+
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $id = mysqli_real_escape_string($con, $_POST['id']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+    $query = "UPDATE students SET name='$name', id='$id', email='$email', password='$password' WHERE id='$student_id' ";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run)
+    {
+        $_SESSION['message'] = "Student Updated Successfully";
+        header("Location: index.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "Student Not Updated";
+        header("Location: index.php");
+        exit(0);
+    }
+
+}
+
+
+if(isset($_POST['save_student']))
+{
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $id = mysqli_real_escape_string($con, $_POST['id']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+
+    $query = "INSERT INTO students (name,id,email,password) VALUES ('$name','$id','$email','$password')";
+
+    $query_run = mysqli_query($con, $query);
+    if($query_run)
+    {
+        $_SESSION['message'] = "Student Created Successfully";
+        header("Location: student-create.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "Student Not Created";
+        header("Location: student-create.php");
+        exit(0);
+    }
+}
+
+?>
